6d2357a8e09ac1f862ce110aa4dc13e4850ac2a2
Damla Koksal <me@damlakoksal.com>
2021-09-20 19:15:17 +0300

    add: pull user and posts data

--- src/lib/service.js
+++ src/lib/service.js
@@ -2,9 +2,16 @@ import axios from "axios";
 
 export default async function getData(userId) {
     try {
-        const { data: user } = await axios(`https://jsonplaceholder.typicode.com/users/${userId}`);
-        console.log(user);
+        // extracted data from the incoming object
+        let { data: user } = await axios(`https://jsonplaceholder.typicode.com/users/${userId}`);
+        let { data: posts } = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+
+        user = {
+            ...user, posts
+        }
+        return await user;
+
     } catch (error) {
-        console.error(error.message);
+        return error.message;
     }
 }
