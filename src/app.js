101300740b07e7f52566d1e27599dbdaafaf915a
Damla Koksal <me@damlakoksal.com>
2021-09-22 17:25:53 +0300

    add: add comments

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
 import getData from './lib/service.js'
 
+// logging incoming data after importing module.
 console.log(await getData(1));
--- src/lib/service.js
+++ src/lib/service.js
@@ -12,12 +12,16 @@ const getData = async (userId) => {
         let { data: user } = await axios(`https://jsonplaceholder.typicode.com/users/${userId}`);
         let { data: posts } = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
 
+        // changed user object according to the data format requirements
         user = {
             ...user, posts
         }
+
+        // returned final user data
         return user;
 
     } catch (error) {
+        // returning error message 
         return error.message;
     }
 }
